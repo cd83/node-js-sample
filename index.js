8e03af077d137a2b618272eb54cd9abe806b32b0
Chris Lee <chris.lee@leankit.com>
2015-11-15 18:36:07 -0600

    Update index.js
    copypasta errors

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
  var express = require('express')
  var app = express()		 var app = express()
  		 
- app.set('port', (process.env.PORT || 5000))		 app.set('port', (process.env.PORT || 5000))
- app.use(express.static(__dirname + '/public'))		 app.use(express.static(__dirname + '/public'))
+ app.set('port', (process.env.PORT || 5000))
+ app.use(express.static(__dirname + '/public'))
  		 
- app.get('/', function(request, response) {		 app.get('/', function(request, response) {
-   response.send('Hello World!')		   response.send('Hello World!')
- })		 })
+ app.get('/', function(request, response) {
+   response.send('Hello World!')
+ })
  		 
- app.listen(app.get('port'), function() {		 app.listen(app.get('port'), function() {
-   console.log("Node app is running at localhost:" + app.get('port'))		   console.log("Node app is running at localhost:" + app.get('port'))
- })		 })
+ app.listen(app.get('port'), function() {
+   console.log("Node app is running at localhost:" + app.get('port'))
+ })	
